6ed69d037c7ba573388aac8df908206e894b70f8
domkoch <dominikoch49@gmail.com>
2017-01-27 11:21:11 +0100

    add in app.js the URL

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express()
 
 app.set("view engine", "pug")
 
-app.get("/", function(req, res){
+app.get("https://domkoch.github.io/domkoch/", function(req, res){
     res.render("index", {title: "This is the title", msg: "this is a msg"})
 })
 
